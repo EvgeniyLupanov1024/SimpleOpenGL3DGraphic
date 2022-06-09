fc83e184cbba5ae5d8f3d4c8e66bd653d062e4b2
Евгений Лупанов <e.lupanov@qsoft.ru>
2022-06-09 16:38:43 +0300

    glef init error

--- window.cpp
+++ window.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 #define GLEW_STATIC
 #include <GL/glew.h>
@@ -15,6 +16,12 @@ int width = 1000;
 int height = 600;
 char title[] = "Potitle";
 
+const GLfloat g_vertex_illuminati[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f,  0.5f, 0.0f,
+};
+
 int main()
 {
     init();
@@ -25,7 +32,7 @@ int main()
 
         glClearColor(0.91f, 0.85f, 0.73f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
+
         glfwSwapBuffers(window);
     }
 
@@ -34,24 +41,29 @@ int main()
 }
 
 void init() 
-{
-    glfwInit();
+{    
+    if (glfwInit() != GLFW_TRUE) {
+        throw std::runtime_error("glfw not init");
+    }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     window = glfwCreateWindow(width, height, title, nullptr, nullptr);
     if (window == nullptr) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        std::exit;
+        throw std::runtime_error("Failed to create GLFW window" );
     }
     glfwMakeContextCurrent(window);
 
     glViewport(0, 0, width, height);
     glfwSetKeyCallback(window, key_callback);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        throw std::runtime_error("glew not init");
+    }
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
