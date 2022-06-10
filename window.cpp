47578542a511513718a7f7ca6bf00568b6b7c44d
Евгений Лупанов <e.lupanov@qsoft.ru>
2022-06-10 12:08:21 +0300

    илюминаты -- набросок шейдоров

--- window.cpp
+++ window.cpp
@@ -16,15 +16,56 @@ int width = 1000;
 int height = 600;
 char title[] = "Potitle";
 
-const GLfloat g_vertex_illuminati[] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
+GLfloat g_vertex_illuminati[] = {
+    -0.3f, -0.5f, 0.0f,
+    0.3f, -0.5f, 0.0f,
     0.0f,  0.5f, 0.0f,
 };
+
+GLchar* vertexShaderSource = (GLchar*) "#version 330 core\n"
+    "layout (location = 0) in vec3 position;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+    "}\0";
+GLchar* fragmentShaderSource = (GLchar*) "#version 330 core\n"
+    "out vec4 color;\n"
+    "void main()\n"
+    "{\n"
+    "color = vec4(0.35f, 0.5f, 0.25f, 1.0f);\n"
+    "}\n\0";
 
 int main()
 {
     init();
+
+    GLuint vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    GLuint VAO; 
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); 
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_illuminati), g_vertex_illuminati, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
 
     while(!glfwWindowShouldClose(window))
     {
@@ -32,6 +73,9 @@ int main()
 
         glClearColor(0.91f, 0.85f, 0.73f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
     }
@@ -43,7 +87,7 @@ int main()
 void init() 
 {    
     if (glfwInit() != GLFW_TRUE) {
-        throw std::runtime_error("glfw not init");
+        throw runtime_error("GLFW not init");
     }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -53,7 +97,7 @@ void init()
 
     window = glfwCreateWindow(width, height, title, nullptr, nullptr);
     if (window == nullptr) {
-        throw std::runtime_error("Failed to create GLFW window" );
+        throw runtime_error("Failed to create GLFW window" );
     }
     glfwMakeContextCurrent(window);
 
@@ -62,7 +106,7 @@ void init()
 
     glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK) {
-        throw std::runtime_error("glew not init");
+        throw runtime_error("GLEW not init");
     }
 }
 
